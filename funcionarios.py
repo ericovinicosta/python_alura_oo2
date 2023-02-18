ebd5e712e8d3cd6f6f8a3f9bce89b822aefccf87
suporteerico <suporte@ericocosta.com.br>
2023-02-17 22:28:46 -0300

    herança em funcionario hipster

--- funcionarios.py
+++ funcionarios.py
@@ -1,8 +1,25 @@
 class Funcionario():
+    def __init__(self, nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         self._horas = horas
+        print('Horas registradas...')
 
-    def mostra_tarefas(self):
-        pass
+    def mostra_tarefas(self, tarefas):
+        for tarefa in tarefas:
+            print(tarefa)
 
-    
+class Alura(Funcionario):
+    pass
+
+class Caelum(Funcionario):
+    pass
+
+class Hipster:
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
+class Senior(Alura, Caelum, Hipster):
+    pass
+
